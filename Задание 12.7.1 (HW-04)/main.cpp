132f69a286216ba1ffe1f89feb3c73de33c21d37
ShadowX <56553389+SzadowX@users.noreply.github.com>
2024-04-10 22:22:13 +0700

    Add files via upload

---
+++ Задание 12.7.1 (HW-04)/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+bool IsKPeriodic(const string& str, int K)
+{
+	if (str.length() % K != 0)
+		return false;
+	for (int i = 0; i < str.length(); i += K)
+	{
+		if (str.substr(i, K) != str.substr(0, K))
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+	string input;
+	cout << "Введите строку: \n";
+	cin >> input;
+	int K;
+	cout << "Введите число K: \n";
+	cin >> K;
+	if (IsKPeriodic(input,K))
+	{
+		cout << "Строка кратна числу K \n";
+	}
+	else
+	{
+		cout << "Строка не кратна числу K \n";
+	}
+	return 0;
+}
